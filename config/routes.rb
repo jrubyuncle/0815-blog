ccd9d910f93d3870a565cfb1cc960e7b6c2d1ddd
odin1in <odin@pwn.so>
2015-08-01 14:53:22 +0800

    add backend dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  namespace :backend do
+    get '/', to: 'dashboard#index'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
